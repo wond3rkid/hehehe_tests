4dd7edcd989386f3f2b33cca363f8593f41fd942
ffr3a <savilovaae@yandex.ry>
2025-03-20 01:14:01 +0700

    init

---
+++ tests/conftest.py
@@ -0,0 +1,22 @@
+import pytest
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+@pytest.fixture(scope="session", autouse=True)
+def riot_api_key():
+    """
+    Возвращает API-ключ и проверяет его наличие.
+    """
+    key = os.getenv("RIOT_API_KEY")
+    if not key:
+        pytest.fail("RIOT_API_KEY не указан в .env")
+    return key
+
+@pytest.fixture(scope="module")
+def base_url():
+    return "https://europe.api.riotgames.com"
+
+
+
